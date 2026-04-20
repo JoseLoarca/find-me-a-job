b2c1cabf62a77e3aa060d117565371d3ccadfb64
Jose Loarca <joseloarca97@icloud.com>
2026-04-20 16:32:55 -0400

    Add support for Ollama-based profile evaluation.

--- core/health.py
+++ core/health.py
@@ -1,6 +1,7 @@
 import os
 
 from pymongo import MongoClient
+import ollama
 
 from logger import get_session_logger
 from models import AppConfig
@@ -8,6 +9,11 @@ from models import AppConfig
 logger = get_session_logger()
 
 def _mongo_health_check() -> bool:
+    """Perform a quick health check on the MongoDB database.
+    The health check consists of a simple ping to the db.
+
+    Returns: True if the health check succeeded, False otherwise.
+    """
     try:
         logger.info("Starting Mongo Health Check")
         client = MongoClient(os.getenv("MONGODB_CONN"), serverSelectionTimeoutMS=3000)
@@ -15,7 +21,23 @@ def _mongo_health_check() -> bool:
         client.close()
         return True
     except Exception as e:
-        logger.error('Mongo health check failed: ', e)
+        logger.error(f"Mongo health check failed: {e}")
+        return False
+
+def _ollama_health_check() -> bool:
+    """Perform a quick health check on the Ollama connection.
+    In order to consider the Ollama configuration "healthy", the following criteria should be met:
+    - Ollama is up and running.
+    - The base model exists in the system.
+
+    Returns: True if the health check succeeded, False otherwise.
+    """
+    try:
+        logger.info("Starting Ollama Health Check")
+        ollama.show(os.getenv("OLLAMA_EVALUATOR_BASE_MODEL"))
+        return True
+    except Exception as e:
+        logger.error(f"Ollama health check failed: {e}")
         return False
 
 
@@ -42,6 +64,9 @@ class HealthChecker:
         # Evaluator health
         if self.config.evaluator_service == "gemini":
             checks["gemini_for_evaluator"] = gemini_available
+
+        if self.config.evaluator_service == "ollama":
+            checks["ollama_for_evaluator"] = _ollama_health_check()
 
         # Storage health
         if self.config.storage_service == "mongodb":
--- core/registry.py
+++ core/registry.py
@@ -1,5 +1,6 @@
 from services.analyze.gemini import GeminiAnalyzer
 from services.evaluate.gemini import GeminiEvaluator
+from services.evaluate.local_llm import OllamaEvaluator
 from storage.mongodb import MongoDBStorage
 
 ANALYZER_REGISTRY = {
@@ -8,6 +9,7 @@ ANALYZER_REGISTRY = {
 
 EVALUATOR_REGISTRY = {
     "gemini": GeminiEvaluator,
+    "ollama": OllamaEvaluator,
 }
 
 STORAGE_REGISTRY = {
